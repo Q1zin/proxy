6a5150c37f34ba9cfa2122022e0c179cd2c18ef3
q1zin <q1zin@mail.ru>
2025-05-09 18:31:41 +0700

    Add hook preservation to greeting plugin

--- plugins/greeting/greeting.c
+++ plugins/greeting/greeting.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include "master.h"
 
+static Hook previous_hook = NULL;
+
 static void greeting_function(void) {
+    if (previous_hook) {
+        previous_hook();
+    }
+
     fprintf(stdout, "Hello, world!\n");
 }
 
 void init(void) {
+    previous_hook = executor_start_hook;
     fprintf(stdout, "greeting initialized\n");
     executor_start_hook = greeting_function;
 }
 
 void fini(void) {
     fprintf(stdout, "greeting finished\n");
-    executor_start_hook = NULL;
+    executor_start_hook = previous_hook;
 }
 
 const char *name(void) {
