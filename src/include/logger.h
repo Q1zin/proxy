ea94866618d7fa3624e3d07954d7d1c3b3eb0c50
q1zin <q1zin@mail.ru>
2025-05-09 18:24:50 +0700

    Refactor logger

--- src/include/logger.h
+++ src/include/logger.h
@@ -1,9 +1,15 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+extern int debug_mode;
+
 int init_logger(char* path, int file_size_limit);
 
 int fini_logger(void);
+
+void set_log_file_path(char* path);
+
+void set_log_file_size_limit(int file_size_limit);
 
 enum LogLevel {
     LOG_DEBUG = 1,
@@ -19,14 +25,7 @@ enum OutputStream {
     FILESTREAM
 };
 
-const char* level_names[] = {
-    "UNKNOWN",
-    "LOG_DEBUG",
-    "LOG_INFO",
-    "LOG_WARNING",
-    "LOG_ERROR",
-    "LOG_FATAL"
-};
+extern const char* level_names[];
 
 int write_log(enum OutputStream stream, enum LogLevel level, char* filename, int line_number, char* format, ...);
 
--- src/logger.c
+++ src/logger.c
@@ -6,11 +6,20 @@
 #include "include/logger.h"
 #include "include/my_time.h"
 
+int debug_mode = 0;
+
 static int __init_count_logger = 0;
 char* __log_file_path = NULL;
 int __file_size_limit = -1;
 
-struct stat st;
+const char* level_names[] = {
+    "UNKNOWN",
+    "LOG_DEBUG",
+    "LOG_INFO",
+    "LOG_WARNING",
+    "LOG_ERROR",
+    "LOG_FATAL"
+};
 
 int init_logger(char* path, int file_size_limit) {
     __log_file_path = path;
@@ -35,11 +44,49 @@ int fini_logger(void) {
     return 1;
 }
 
+void set_log_file_path(char* path) {
+    __log_file_path = path;
+}
+
+void set_log_file_size_limit(int file_size_limit) {
+    __file_size_limit = file_size_limit;
+}
+
 char* get_utc(char* buffer, size_t buffer_size) {
     time_t time = get_time();
     struct tm* time_info = gmtime(&time);
     strftime(buffer, buffer_size, "%Y-%m-%dT%H:%M:%S(UTC)", time_info);
     return buffer;
+}
+
+void log_to_stream(FILE* stream, const char* time_buffer, const char* filename, int line_number, int pid, const char* level_str, const char* format, va_list args) {
+    fprintf(stream, "%s %s %d [%d] | %s: ", time_buffer, filename, line_number, pid, level_str);
+    vfprintf(stream, format, args);
+    fprintf(stream, "\n");
+}
+
+void log_to_file(const char* time_buffer, const char* filename, int line_number, int pid, const char* level_str, const char* format, va_list args) {
+    int fd = open(__log_file_path, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        log_to_stream(stderr, time_buffer, filename, line_number, pid, level_str, format, args);
+        return;
+    }
+
+    struct stat st;
+    fstat(fd, &st);
+    off_t size_kb = (st.st_size + 1023) / 1024;
+
+    char buffer[1024];
+    int msg_size = vsnprintf(buffer, sizeof(buffer), format, args);
+
+    if (__file_size_limit != -1 && size_kb + (msg_size / 1024) >= __file_size_limit) {
+        if (ftruncate(fd, 0) != -1) {
+            lseek(fd, 0, SEEK_SET);
+        }
+    }
+
+    dprintf(fd, "%s %s %d [%d] | %s: %s\n", time_buffer, filename, line_number, pid, level_str, buffer);
+    close(fd);
 }
 
 // "TIME(UTC) FILE LINE [PID] | LEVEL: MESSAGE"
@@ -54,39 +101,12 @@ int write_log(enum OutputStream stream, enum LogLevel level, char* filename, int
     va_list args;
     va_start(args, format);
 
-    if (stream == STDOUT) {
-        printf("%s %s %d [%d] | %s: ", time_buffer, filename, line_number, pid, level_str);
-        vprintf(format, args);
-        printf("\n");
+    if (debug_mode || stream == STDOUT) {
+        log_to_stream(stdout, time_buffer, filename, line_number, pid, level_str, format, args);
     } else if (stream == FILESTREAM && __log_file_path != NULL) {
-        int fd = open(__log_file_path, O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-        if (fd == -1) {
-            fprintf(stderr, "%s %s %d [%d] | %s: ", time_buffer, filename, line_number, pid, level_str);
-            vfprintf(stderr, format, args);
-            fprintf(stderr, "\n");
-            va_end(args);
-            return 0;
-        }
-
-        fstat(fd, &st);
-        off_t size_kb = (st.st_size + 1023) / 1024;
-
-        char buffer[1024];
-        int msg_size = vsnprintf(buffer, sizeof(buffer), format, args);
-
-        if (__file_size_limit != -1 && size_kb + (msg_size / 1024) >= __file_size_limit) {
-            ftruncate(fd, 0);
-            lseek(fd, 0, SEEK_SET);
-        }
-
-        dprintf(fd, "%s %s %d [%d] | %s: ", time_buffer, filename, line_number, pid, level_str);
-        dprintf(fd, "%s\n", buffer);
-
-        close(fd);
+        log_to_file(time_buffer, filename, line_number, pid, level_str, format, args);
     } else {
-        fprintf(stderr, "%s %s %d [%d] | %s: ", time_buffer, filename, line_number, pid, level_str);
-        vfprintf(stderr, format, args);
-        fprintf(stderr, "\n");
+        log_to_stream(stderr, time_buffer, filename, line_number, pid, level_str, format, args);
     }
     
     va_end(args);
